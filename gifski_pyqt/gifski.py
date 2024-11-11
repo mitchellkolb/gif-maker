b5316844274eaca3eff25d73b9ea3e2bfc8e92fc
Mitchell Kolb <66702995+mitchellkolb@users.noreply.github.com>
2024-11-10 20:56:46 -0800

    Added py03 memory release code to gifski.py

--- gifski_pyqt/gifski.py
+++ gifski_pyqt/gifski.py
@@ -8,3 +8,8 @@ print(f"add_numbers(5, 3) = {result}")  # Expected output: 8
 # Test generate_gif_message function
 message = gifski_pyqt.generate_gif_message()
 print(f"generate_gif_message() = {message}")  # Expected output: "GIF generated successfully!"
+
+
+# end tbe fucntion to release rust memory
+gifski_pyqt.release()
+gifski_pyqt.quit()
